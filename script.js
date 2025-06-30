f1671fdf401b350df97e343f625ce4c1c7fb083a
YERED-1 <q00860g@upla.edu.pe>
2025-06-30 15:16:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const logoAzul = document.getElementById('logo-azul');
+  const logoBlanco = document.getElementById('logo-blanco');
+  // Código JS aquí
+});
